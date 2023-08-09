96c48dd09f5ce95992d3a603254bb314dd2cc1f3
moshiur <55761320+moshiur-ux@users.noreply.github.com>
2023-08-09 23:38:17 +0600

    add somentings

--- Assianment.js
+++ Assianment.js
@@ -64,6 +64,13 @@ function findAddress(obj){
     if(typeof obj !== "object"){
         return "please provide me a valid object"
     }else{
+
+        for (const key in obj) {
+            if (Object.hasOwnProperty.call(obj, key)) {
+                const element = obj[key];
+                
+            }
+        }
         const  street=obj. street || "__";
         const house= obj.house || "__";
         const  society = obj. society || "__";
